b8a98ae19989ae4c52857c1f061dd81eebc2b9e6
danangyudhatama <danang.yudhatama@gmail.com>
2017-09-25 03:33:22 +0200

    solve the bug about the color picker and clean up the code a little bit

--- js/designs.js
+++ js/designs.js
@@ -1,29 +1,26 @@
-// Select color input
-// Select size input
-
 // When size is submitted by the user, call makeGrid()
-
-
-$("#sizePicker").submit(function makeGrid() {
+$("#sizePicker").submit(function makeGrid(e) {
 
 	$("tr").remove();
 	$("td").remove();
-	
+	// Select size input
 	let height = $("#input_height").val();
 	let width = $("#input_width").val();
 	
-		for (let i = height - 1; i >= 0; i--) {
-			$("table").append("<tr></tr>\n");
-				for (let j = width - 1; j >= 0; j--) {
-					$("table tr:last-child").append("<td></td>\n");
-				}
-		}
-
-	let color = $("#colorPicker").val();
+	for (let i = height - 1; i >= 0; i--) {
+		$("table").append("<tr></tr>\n");
+		for (let j = width - 1; j >= 0; j--) {
+			$("table tr:last-child").append("<td></td>\n");
+		}	
+	}
+		// Change the color of the grid
 	$("td").click(function(){
+		// Select color input
+		let color = $("#colorPicker").val();
 		$(this).css("background-color",color);
 	})
-
-	return false;
+	
+	return e.preventDefault();
 })
+
 
